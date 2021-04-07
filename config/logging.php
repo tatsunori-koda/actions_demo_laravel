e135ae487743d3182d7b43f4b96e5f7a6920f23f
t.koda <t.koda0426@gmail.com>
2021-04-07 12:21:52 +0900

    # laravel定義変更

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,8 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            // 'channels' => ['single'],
+            'channels' => ['stderr'],
             'ignore_exceptions' => false,
         ],
 
