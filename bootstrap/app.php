6b931553ee39ff252f3e44b4a853213a232b67f9
t.koda <t.koda0426@gmail.com>
2021-04-07 14:44:03 +0900

    serverless対応

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+// Serverlessで追加
+$app->useStoragePath($_ENV['APP_STORAGE'] ?? $app->storagePath());
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
