a6403151ea887c1be6401204a4d035933640e23c
t.koda <t.koda0426@gmail.com>
2021-04-08 15:13:44 +0900

    handler

---
+++ handler.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports.auth = (event, context, callback) => {
+    var token = event.headers["Authorization"];
+    if (token == 'password') {
+        callback(null, {
+            principalId: 'user',
+            policyDocument: {
+                Version: '2012-10-17',
+                Statement: [{
+                    Action: 'execute-api:Invoke',
+                    Effect: 'Allow',
+                    Resource: "*",
+                }]
+            }
+        });
+    } else {
+        callback('Unauthorized');
+    }
+};
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
