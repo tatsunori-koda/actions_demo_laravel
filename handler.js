f34480cc88011889f75e42c8ede2899b7860b70c
t.koda <t.koda0426@gmail.com>
2021-04-09 10:07:48 +0900

    handler.js修正

--- handler.js
+++ handler.js
@@ -2,24 +2,53 @@
 
 module.exports.auth = (event, context, callback) => {
     let Authorization = event.headers.Authorization;
+
     if (!Authorization) return callback('Unauthorized');
-    let [username, password] = (new Buffer(Authorization.split(' ')[1], 'base64')).toString().split(':');
-    if (username === 'admin' && password === 'password') {
-        callback(null, {
-            principalId: 'user',
-            policyDocument: {
-                Version: '2012-10-17',
-                Statement: [{
-                    Action: 'execute-api:Invoke',
-                    Effect: 'Allow',
-                    Resource: "*",
-                }]
-            }
-        });
-    } else {
-        callback('Unauthorized');
-    }
+
+    var encodedCreds = authorizationHeader.split(" ")[1]
+    var plainCreds = (new Buffer(encodedCreds, 'base64')).toString().split(':')
+    var username = plainCreds[0]
+    var password = plainCreds[1]
+
+    if (!(username === 'admin' && password === 'password')) return callback('Unauthorized')
+
+    var authResponse = buildAllowAllPolicy(event, username)
+
+    callback(null, authResponse)
+
+    // if (username === 'admin' && password === 'password') {
+    //     callback(null, {
+    //         principalId: 'user',
+    //         policyDocument: {
+    //             Version: '2012-10-17',
+    //             Statement: [{
+    //                 Action: 'execute-api:Invoke',
+    //                 Effect: 'Allow',
+    //                 Resource: "*",
+    //             }]
+    //         }
+    //     });
+    // } else {
+    //     callback('Unauthorized');
+    // }
 };
+
+function buildAllowAllPolicy (event, principalId) {
+  const policy = {
+    principalId: principalId,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: 'Allow',
+          Resource: event.methodArn
+        }
+      ]
+    }
+  }
+  return policy
+}
 
 module.exports.hello = async event => {
   return {
