513f7c3c611f1aaa197698ab6980f1e73cf33626
t.koda <t.koda0426@gmail.com>
2021-04-15 10:52:57 +0900

    どやさ

--- non_authorizer.js
+++ non_authorizer.js
@@ -1,39 +1,18 @@
 // Authorizationをせずに通過させる
-module.exports.handler = (event, context, callback) => {
-  const [ identifier, service, action, region, accountId, apiGatewayArn ] = event.methodArn.split(':')
-  const [ apiId, stage, ..._rest ] = apiGatewayArn.split('/')
-  callback(null, {
+module.exports.handler = (event, context) => {
+  const policy = {
     principalId: 'user',
-    policyDocument: {
-        Version: '2012-10-17',
-        Statement: [{
-            Action: 'execute-api:Invoke',
-            Effect: 'Allow',
-            Resource: [`${identifier}:${service}:${action}:${region}:${accountId}:${apiId}/${stage}/*/*`]
-        }]
-    }
-  });
-}
-
-// IAMポリシーの作成
-function buildPolicy (event, principalId) {
-  const [ identifier, service, action, region, accountId, apiGatewayArn ] = event.methodArn.split(':')
-  const [ apiId, stage, ..._rest ] = apiGatewayArn.split('/')
-
-  return {
-    principalId,
     policyDocument: {
       Version: '2012-10-17',
       Statement: [
         {
-          // 実行アクション
           Action: 'execute-api:Invoke',
-          // 許可
           Effect: 'Allow',
-          // API Gatewayエンドポイント
+          // Resource: event.methodArn,
           Resource: [`${identifier}:${service}:${action}:${region}:${accountId}:${apiId}/${stage}/*/*`]
-        }
-      ]
-    }
-  }
+        },
+      ],
+    },
+  };
+  context.succeed(policy);
 }
