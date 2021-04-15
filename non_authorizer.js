d73ff2369602baf5a845e1e8b6fb1110e74704ca
t.koda <t.koda0426@gmail.com>
2021-04-15 12:01:50 +0900

    ちょっと一旦冷静に

--- non_authorizer.js
+++ non_authorizer.js
@@ -1,12 +1,17 @@
 // Authorizationをせずに通過させる
-module.exports.handler = async(event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+module.exports.handler = (event, context, callback) => {
+  callback(null, {
+    principalId: 'user',
+    policyDocument: {
+        Version: '2012-10-17',
+        Statement: [{
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: event.methodArn,
+            // Resource: [`${identifier}:${service}:${action}:${region}:${accountId}:${apiId}/${stage}/*/*`]
+        }]
+    }
+  });
 }
 
 // module.exports.handler = (event, context, callback) => {
