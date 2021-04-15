73112a0e7345ea480f9b44f95595bcfa9dc4b907
t.koda <t.koda0426@gmail.com>
2021-04-15 11:35:46 +0900

    もういっちょ

--- non_authorizer.js
+++ non_authorizer.js
@@ -1,7 +1,6 @@
 // Authorizationをせずに通過させる
-module.exports.handler = (event, context, callback) => {
-  var token = event.headers["Authorization"];
-  callback(null, {
+module.exports.handler = (event) => {
+  return {
     principalId: 'user',
     policyDocument: {
         Version: '2012-10-17',
@@ -12,5 +11,21 @@ module.exports.handler = (event, context, callback) => {
             // Resource: [`${identifier}:${service}:${action}:${region}:${accountId}:${apiId}/${stage}/*/*`]
         }]
     }
-  });
+  }
 }
+
+// module.exports.handler = (event, context, callback) => {
+//   var token = event.headers["Authorization"];
+//   callback(null, {
+//     principalId: 'user',
+//     policyDocument: {
+//         Version: '2012-10-17',
+//         Statement: [{
+//             Action: 'execute-api:Invoke',
+//             Effect: 'Allow',
+//             Resource: event.methodArn,
+//             // Resource: [`${identifier}:${service}:${action}:${region}:${accountId}:${apiId}/${stage}/*/*`]
+//         }]
+//     }
+//   });
+// }
