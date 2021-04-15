a524849945a6322052e0e419a21b6080373d0010
t.koda <t.koda0426@gmail.com>
2021-04-15 11:08:03 +0900

    のんおーそ

--- non_authorizer.js
+++ non_authorizer.js
@@ -1,18 +1,15 @@
 // Authorizationをせずに通過させる
-module.exports.handler = (event, context) => {
-  const policy = {
+module.exports.handler = (event, context, callback) => {
+  callback(null, {
     principalId: 'user',
     policyDocument: {
-      Version: '2012-10-17',
-      Statement: [
-        {
-          Action: 'execute-api:Invoke',
-          Effect: 'Allow',
-          // Resource: event.methodArn,
-          Resource: [`${identifier}:${service}:${action}:${region}:${accountId}:${apiId}/${stage}/*/*`]
-        },
-      ],
-    },
-  };
-  context.succeed(policy);
+        Version: '2012-10-17',
+        Statement: [{
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            // Resource: event.methodArn,
+            Resource: [`${identifier}:${service}:${action}:${region}:${accountId}:${apiId}/${stage}/*/*`]
+        }]
+    }
+  });
 }
